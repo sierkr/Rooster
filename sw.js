90c5f3037b09593f37ab4a0d2c25c30b691d59c6
sierkr <sierkr@gmail.com>
2026-05-07 13:35:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache-naam bevat versienummer. Bij een nieuwe versie worden oude caches
 // automatisch verwijderd en alle bestanden opnieuw gecached.
 
-const VERSION = '3.27.43';
+const VERSION = '3.27.44';
 const CACHE = `rooster-${VERSION}`;
 
 const PRECACHE = [
