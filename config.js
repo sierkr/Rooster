a727e27c4168d0e3753be8efea5bde9ddf6b3d23
sierkr <sierkr@gmail.com>
2026-06-02 17:17:24 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,2 @@
-// ============================================================================
-// Firebase configuratie
-// ============================================================================
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCIp8T0-BNPlh3j9X2QbXkQsaq7F91xoOA",
-  authDomain: "rooster-radiologie.firebaseapp.com",
-  projectId: "rooster-radiologie",
-  storageBucket: "rooster-radiologie.firebasestorage.app",
-  messagingSenderId: "798466630775",
-  appId: "1:798466630775:web:65252d0d0a606ab9141272"
-};
-
-// Versie van de app - wordt gebruikt voor cache-busting
-window.APP_VERSIE = "3.27.95";
+node_modules/
+*.local
